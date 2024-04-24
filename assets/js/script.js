12d64729ca8d5e41b1d68fb09141b5afc2259248
Aldiva Wibowo <90434857+Aldbow@users.noreply.github.com>
2024-04-24 08:10:53 +0700

    feat: :art: improve style and reduce animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,19 @@
 
 const animate = document.querySelectorAll(".animate");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+  { once: true }
+);
 
 animate.forEach((section) => {
   section.classList.add("hidden");
